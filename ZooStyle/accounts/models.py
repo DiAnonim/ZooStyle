54a7750006cf78f08b2330e8d382749877a8ab1a
DiAnonim <akopyan.2002@mail.ru>
2025-07-16 13:58:39 +0500

    Create Model CustomUser install phonenumbers library 'pip install phonenumbers' add file validators.py and create function validate_phone_number

--- ZooStyle/accounts/models.py
+++ ZooStyle/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import Abstract
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
+from accounts.validators import validate_phone_number
+
+class CustomUser(AbstractUser):
+    name = models.CharField(_("Name"), max_length=150)
+    phone = models.IntegerField(_("Phone number"), max_length=20, validators=[validate_phone_number])
+    address = models.CharField(_("Address"), max_length=255, blank=True, null=True)
+    photo = models.ImageField(_("Photo"), upload_to='accounts/profile_photo/', blank=True, null=True)
+    pet_name = models.CharField(_("Pet name"), max_length=100, blank=True, null=True)
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ["name", "phone"]
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
---
+++ ZooStyle/accounts/validators.py
@@ -0,0 +1,11 @@
+import phonenumbers
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+
+def validate_phone_number(value):
+    try:
+        number = phonenumbers.parse(value, "RU")
+        if not phonenumbers.is_valid_number(number):
+            raise ValidationError(_("Invalid phone number."))
+    except phonenumbers.phonenumberutil.NumberParseException:
+        raise ValidationError(_("Invalid phone number format."))
