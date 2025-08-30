4aad14d9230f3fd6cea99d34d902fdf895fbc6eb
DiAnonim <akopyan.2002@mail.ru>
2025-08-30 15:37:12 +0500

    Adding in static folders to all apps, displaying a photo in the user's profile

--- ZooStyle/accounts_app/models.py
+++ ZooStyle/accounts_app/models.py
@@ -7,7 +7,7 @@ from accounts_app.validators import validate_phone_number
 class CustomUser(AbstractUser):
     phone = models.CharField(_("Phone number"), max_length=20, validators=[validate_phone_number])
     address = models.CharField(_("Address"), max_length=255, blank=True, null=True)
-    photo = models.ImageField(_("Profile photo"), default='accounts_app/images/default_profile_pic.png')
+    photo = models.ImageField(_("Profile photo"), upload_to='accounts_app/profile_photo/', default='accounts_app/profile_photo/default_profile_pic.png')
     pet_name = models.CharField(_("Pet name"), max_length=100, blank=True, null=True)
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ["first_name", "phone"]
